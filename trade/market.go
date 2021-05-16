b31dbc6c7f4959dbf255eea578749e86ee0f6777
ubuntu <ubuntu@localhost.localdomain>
2021-05-16 12:17:44 +0800

    s

--- trade/market.go
+++ trade/market.go
@@ -1,9 +1,40 @@
 package trade
 
-func (hm *HuobiMarket) GetCandleStick() {
+import "github.com/huobirdcenter/huobi_golang/pkg/client"
+
+//不同的交易中心
+type MarketInfo interface {
+	GetAllSymbols()
+}
+
+//Huobi交易中心
+type HuobiMarket struct {
+	Client *client.CommonClient
+}
+
+func (hm *HuobiMarket) GetAllSymbols() {
+	hm.Client.GetSymbols()
 
 }
 
-func (hm *HuobiMarket) GetBCN() {
+/////////////////////////////////////////////
+//不同的交易中心
+type StockInfo interface {
+	GetLast24hCandlestick()
+	GetCandleStick()
+	GetBCN()
+}
+
+//Huobi股票
+type HuobiStock struct {
+	Client *client.MarketClient
+	Stock  string
+}
+
+func (hs *HuobiStock) GetCandleStick() {
+
+}
+
+func (hs *HuobiStock) GetBCN() {
 
 }
